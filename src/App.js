99cf68d94a5eae740b2e79718de6f744fbdcd6ab
hoangnk <hoangkimzxc@gmail.com>
2022-04-30 20:14:06 +0700

    eleventh-cm

--- src/App.js
+++ src/App.js
@@ -1,120 +1,16 @@
 import "./App.css";
-import { useState } from "react";
 
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
-
-const FilterableProductTable = ({ products }) => {
-  const [filterText, setFilterText] = useState("");
-  const [inStockOnly, setInStockOnly] = useState(false);
-
-  return (
-    <div>
-      <SearchBar
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-        onFilterTextChange={setFilterText}
-        onInStockOnlyChange={setInStockOnly}
-      />
-      <ProductTable
-        products={products}
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-      />
-    </div>
-  );
-};
-
-const ProductCategoryRow = ({ category }) => (
-  <tr>
-    <th colSpan="2">{category}</th>
-  </tr>
-);
-
-const ProductRow = ({ product }) => {
-  const name = product.stocked ? (
-    product.name
-  ) : (
-    <span style={{ color: "red" }}>{product.name}</span>
-  );
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-};
-
-const ProductTable = ({ products, filterText, inStockOnly }) => {
-  const rows = [];
-  let lastCategory = null;
-  products.forEach((product) => {
-    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
-      return;
-    }
-    if (inStockOnly && !product.stocked) {
-      return;
-    }
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category}
-        />
-      );
-    }
-    rows.push(<ProductRow product={product} key={product.name} />);
-    lastCategory = product.category;
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-};
-
-const SearchBar = ({
-  filterText,
-  inStockOnly,
-  onFilterTextChange,
-  onInStockOnlyChange,
-}) => (
-  <form>
-    <input
-      type="text"
-      placeholder="Search..."
-      value={filterText}
-      onChange={(e) => onFilterTextChange(e.target.value)}
-    />
-    <label>
-      <input
-        type="checkbox"
-        checked={inStockOnly}
-        onChange={(e) => onInStockOnlyChange(e.target.checked)}
-      />
-      {""} Only show products in stock
-    </label>
-  </form>
+const Profile = () => (
+  <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
 );
 
 export default function App() {
   return (
     <div className="avatar">
-      <FilterableProductTable products={PRODUCTS} />
+      <h1>Amazing scientist </h1>
+      <Profile/>
+      <Profile/>
+      <Profile/>
     </div>
   );
 }
