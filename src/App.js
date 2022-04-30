a9835ff4c18f5da39c5c497a572923c901fcb186
hoangnk <hoangkimzxc@gmail.com>
2022-04-30 09:24:28 +0700

    seventh-cm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import { useState } from "react";
 
-const MyButton = () => {
-  const [count, setCount] = useState(0);
-  const handleClick = () => setCount(count + 1);
-  return <button onClick={handleClick}>Clicked {count} times</button>;
-};
+const MyButton = ({ count, onClick }) => (
+  <button onClick={onClick}>Click {count} times</button>
+);
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
   return (
     <div className="avatar">
       <h1>Counter</h1>
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
