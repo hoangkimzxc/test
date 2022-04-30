48af1a7a25d7c310a187a68efb98ac844177c12a
hoangnk <hoangkimzxc@gmail.com>
2022-04-30 09:53:26 +0700

    eighth-cm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import "./App.css";
 import { useState } from "react";
 
-const MyButton = ({ count, onClick }) => (
-  <button onClick={onClick}>Click {count} times</button>
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+const ProductCategoryRow = ({ category }) => (
+  <tr>
+    <th>{category}</th>
+  </tr>
 );
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => setCount(count + 1);
-  return (
-    <div className="avatar">
-      <h1>Counter</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
+const ProductRow = ({ product }) => {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
   );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+};
+
+const ProductTable = ({ products }) => {
+  const rows = [];
+  let lastCategory = null;
+  products.forEach((product) => {
+    if (product !== lastCategory) {
+      rows.push(<ProductCategoryRow category={product.category} />);
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+export default function App() {
+  return <div className="avatar"></div>;
 }
