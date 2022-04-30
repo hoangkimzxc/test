ecb51655a234a660085ecf480598c926c1df56cf
hoangnk <hoangkimzxc@gmail.com>
2022-04-30 08:34:57 +0700

    fourth-cm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-const Greeting = ({ name }) => <h1>Hello {name}</h1>;
+import "./App.css";
+
+const user = {
+  name: "Sigmund Freud",
+  imgUrl: "https://picsum.photos/200",
+  imgSize: 500,
+};
+
+const check=false;
 
 export default function App() {
-  return <Greeting name='Hoang'/>
+  return check &&(
+    <div className="avatar">
+      <h1>{user.name}</h1>
+      <img
+        src={user.imgUrl}
+        style={{ width: user.imgSize, height: user.imgSize }}
+        alt={"userfdsa" + user.name}
+      />
+    </div>
+  );
 }
-
