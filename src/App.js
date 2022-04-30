c88a519b26e07fe1dc34b2c21779b0273596b181
hoangnk <hoangkimzxc@gmail.com>
2022-04-30 21:05:04 +0700

    fifteenth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import "./App.css";
+import { getImageUrl } from "./utils";
 
-const person = {
-  name: "Gregorio Y. Zara",
-  theme: {
-    backgroundColor: "black",
-    color: "pink",
-  },
-};
+const Avatar = ({ person, size }) => (
+  <img
+    className="ava"
+    src={getImageUrl(person)}
+    alt={person.name}
+    width={size}
+    height={size}
+  />
+);
+
+const Card = ({ children }) => <div className="card">{children}</div>;
 
 export default function App() {
   return (
     <div className="avatar">
-      <div style={person.theme}>
-        <h1>{person.name}'s Todos</h1>
-      <img
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-        className="photo"
-      />
-       <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-      </div>
+      <Card>
+        <Avatar
+          size={100}
+          person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }}
+        />
+      </Card>
     </div>
   );
 }
