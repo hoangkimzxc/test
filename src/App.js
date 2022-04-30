a85bff78e481eeece09c0f357f36fbdc86d6810a
hoangnk <hoangkimzxc@gmail.com>
2022-04-30 09:19:13 +0700

    sĩxth-cm

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
-const products = [
-  { title: "Cabbage", isFruit: false, id: 1 },
-  { title: "Garlic", isFruit: false, id: 2 },
-  { title: "Apple", isFruit: true, id: 3 },
-];
-
-const Render = () => {
-  const listItems = products.map((product) => (
-    <li
-      key={product.id}
-      style={{ color: product.isFruit ? "magenta" : "darkgreen" }}
-    >
-      {product.title}
-    </li>
-  ));
-  return <ul>{listItems}</ul>;
+const MyButton = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 };
-
-function handleClick() {
-  alert("What are u clicking at?");
-}
 
 export default function App() {
   return (
     <div className="avatar">
-      <button onClick={handleClick}>CLICKER</button>
-      <Render />
+      <h1>Counter</h1>
+      <MyButton />
+      <MyButton />
     </div>
   );
 }
