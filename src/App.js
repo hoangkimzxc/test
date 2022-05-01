983f95adb500672e06b677246a4e07c9ee638b0a
hoangnk <hoangkimzxc@gmail.com>
2022-05-01 07:49:06 +0700

    nineteenth-cm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 
-let guest=0;
-
-const Cup=()=>{
-  guest=guest+1;
-  return <h2>Tea cup for guest #{guest}</h2>
-}
+const Cup = ({ guest }) => {
+  return <h2>Tea cup for guest #{guest}</h2>;
+};
 
 export default function App() {
- 
   return (
     <div className="avatar">
-      <Cup/>
-      <Cup/>
-      <Cup/>
-      <Cup/>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+      <Cup guest={4} />
     </div>
   );
 }
