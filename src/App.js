88707e0c8aabf34afb32c22e684bf73d3b4a445c
hoangnk <hoangkimzxc@gmail.com>
2022-05-02 08:11:02 +0700

    twenty-third-cm

--- src/App.js
+++ src/App.js
@@ -1,49 +1,40 @@
-import { sculptureList } from "./data.js";
 import { useState } from "react";
 
 export default function App() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowmore] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-
-  const handlePrevClick = () => {
-    if (index === 0) {
-      return setIndex(sculptureList.length - 1);
-    }
-    return setIndex(index - 1);
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
   };
 
-  const handleNextClick = () => {
-    if (index === sculptureList.length - 1) {
-      return setIndex(0);
-    }
-    return setIndex(index + 1);
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
   };
 
-  const handleMoreClick = () => {
-    setShowmore(!showMore);
+  const handleReset = () => {
+    setFirstName("");
+    setLastName("");
   };
 
-  let sculpture = sculptureList[index];
   return (
     <div>
-      <button onClick={handlePrevClick}>Prev</button>
-      <button onClick={handleNextClick}>Next</button>
-      <h2>
-        <i>
-          {sculpture.name} by{sculpture.artist}
-        </i>
-      </h2>
-      <h3>
-        {index + 1} of {sculptureList.length}
-      </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <button onClick={handleMoreClick} disabled={!hasNext || !hasPrev}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          placeholder="First name"
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+        <input
+          placeholder="Last name"
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+        <h1>
+          Hi, {firstName} {lastName}
+        </h1>
+        <button onClick={handleReset}>Reset</button>
+      </form>
     </div>
   );
 }
