e25722c34d51c0798eff4a7b7288175c36133c81
hoangnk <hoangkimzxc@gmail.com>
2022-05-02 08:27:31 +0700

    twenty-fourth-cm

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+export default function FeedbackForm() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('');
 
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleLastNameChange = (e) => {
-    setLastName(e.target.value);
-  };
-
-  const handleReset = () => {
-    setFirstName("");
-    setLastName("");
-  };
-
-  return (
-    <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          placeholder="First name"
-          value={firstName}
-          onChange={handleFirstNameChange}
+  if (isSent) {
+    return <h1>Thank you!</h1>;
+  } else {
+    return (
+      <form onSubmit={e => {
+        e.preventDefault();
+        alert(`Sending: "${message}"`);
+        setIsSent(true);
+      }}>
+        <textarea
+          placeholder="Message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
         />
-        <input
-          placeholder="Last name"
-          value={lastName}
-          onChange={handleLastNameChange}
-        />
-        <h1>
-          Hi, {firstName} {lastName}
-        </h1>
-        <button onClick={handleReset}>Reset</button>
+        <br />
+        <button type="submit">Send</button>
       </form>
-    </div>
-  );
+    );
+  }
 }
